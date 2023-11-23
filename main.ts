cbb39722c872686e98855fe93d9dacb3b0f62549
Chunji Wang <chunjiwang@samsung.com>
2023-11-22 16:04:58 -0800

    First commit: merge two dictionaries

--- main.ts
+++ main.ts
@@ -1,81 +1,102 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DictSettings {
+	toLowerCase: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DictSettings = {
+	toLowerCase: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DictPlugin extends Plugin {
+	settings: DictSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('book-copy', 'Sync Custom Dictionary', (evt: MouseEvent) => {
+			this.syncDict();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'sync',
+			name: 'Sync Custom Dictionary',
+			callback: () => this.syncDict(),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
+		
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new DictSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
-
+		
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+	
+	async syncDict() {
+		
+		// load 'Custom Dictionary.md'
+		const md = this.app.vault.getAbstractFileByPath('Custom Dictionary.md');
+		if (!(md instanceof TFile)) {
+			new Notice('Failed to read note "Custom Dictionary"');
+			return;
+		}
+		const mdlist = await this.app.vault.read(md);
+		const mdwords = mdlist.split('\n').map(word => word.trim());
+		const set: Set<string> = new Set(mdwords);
+		set.delete('');
+		
+		// load 'Custom Dictionary.txt' 
+		const input = document.createElement('input');
+		input.type = 'file';
+		input.click();
+		const fileListOrNull = await new Promise<FileList | null>((resolve) => {
+			input.addEventListener('change', () => {
+				resolve(input.files);
+			});
+		});
+		if (!fileListOrNull) {
+			return;
+		}
+		// Convert FileList to an array
+		const files = Array.from(fileListOrNull);
+		if (files.length < 1) {
+			return;
+		}
+		const txt = files[0];
+		// Read the content of the selected file as a string
+		const txtlist = await this.readFileAsText(txt);
+		txtlist.split('\n').map(word => {
+			if (!word) return;
+			if (word.startsWith('checksum')) return;
+			set.add(word);
+		});
+		console.log(set);
+	}
+
+	async readFileAsText(file: File): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.onload = (event) => {
+				const result = event.target?.result as string;
+				resolve(result);
+			};
+			reader.onerror = (event) => {
+				reject(new Error(`Error reading file: ${event.target?.error?.message}`));
+			};
+			// Read the file as text
+			reader.readAsText(file);
+		});
 	}
 
 	onunload() {
@@ -91,26 +112,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class DictSettingTab extends PluginSettingTab {
+	plugin: DictPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DictPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +126,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('To Lower Case')
+			.setDesc('Convert first-letter-capped words to lower case')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.toLowerCase)
+                .onChange(async (value) => {
+                    this.plugin.settings.toLowerCase = value;
+                    await this.plugin.saveSettings();
+                }));
 	}
 }
